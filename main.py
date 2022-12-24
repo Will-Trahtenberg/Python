de5e09bb93b532ded85a457c82c66ad449a36219
Lenar <lenar.shamsiyarov.04@gmail.com>
2022-12-24 14:17:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,15 @@
-class Actor():
-    def __init__(self, name, amplua, rost):
-        self.rost = rost
-        self.name = name
-        self.amplua = amplua
-        self.count = 0
-    def info(self):
-        return 'Actor ' + self.name + self.amplua
-    def joke(self):
-        if self.count == 0:
-                self.count = 1
-                return 'неудачная'
-        else:
-            self.count = 0
-            return 'удачная'
-def buffenoory(self, size):
-        if size > self.rost:
-            return "влез"
-        else:
-            return "не влез"
-    def change_amplua(self, value, register):
-        amplua = self.amplua
-            if register == 'up':
-                amplua = amplua.upper()
-            return amplua.upper()
-        elif register == 'low':
-            amplua = amplua.lower()
-            return amplua.lower()
-A = Actor('Pulcinella ', 'stupid servant ', 160)
-print(A.info())
-print(A.joke())
-print(A.joke())
-print(A.joke())
-print(A.buffenoory(161))
-A.change_amplua('smart servant', 'up')
-print(A.info())
+a = {12:23, 2:35, 36:90, 4:64,5:11}
+sorted_values = sorted(a.values())
+sorted_a = {} #пустой словарь
+sorted_b = {} #пустой словарь
+for i in sorted_values:#для каждого элемента в отсортированном словаре
+    for k in a.keys():#для ключей в словаре в
+        if a[k] == i:#если ключ а равен значение
+            sorted_a[k] = a[k]#сортируем 
+print(sorted_a)
+b = sorted(a.values(),  reverse = True)#создаем переменную с отсортированным словарем
+for i in b:
+    for k in a.keys():
+        if a[k] == i:#если ключ равен значению
+            sorted_b[k] = a[k]#даем значение
+print(sorted_b)
